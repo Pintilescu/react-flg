ffdd861e99d41f5645d231e7e0faccdb8d1303a9
Marius Pintilie <mariusadrianpintilie033@gmail.com>
2026-02-11 21:16:33 +0200

    WIP

--- apps/dashboard/app/dashboard/flags/page.tsx
+++ apps/dashboard/app/dashboard/flags/page.tsx
@@ -14,7 +14,7 @@ function badgeHelper(slug: string) {
       return 'bg-yellow-100 text-yellow-700 border-yellow-300';
     case 'prod':
       return 'bg-red-100 text-red-700 border-red-300';
-    case 'default':
+    default:
       return 'bg-gray-100 text-gray-700 border-gray-300';
   }
 }
